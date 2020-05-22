c4d993d4ca402db6f1660ec05db857682eaa942f
luis gonzalez <luisgonzalez18997@gmail.com>
2020-05-22 15:43:35 -0400

    add avatar page component

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,7 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1018943227791982592/URnaMrya.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right:10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/original.gif'),
+          image: NetworkImage('https://peru21.pe/resizer/c--Tut_j_a-lWM111FePxDDZ7QQ=/980x528/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/4QQVMIFAR5EQHLPHBHGBLSXJDU.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
+
     );
   }
 }
